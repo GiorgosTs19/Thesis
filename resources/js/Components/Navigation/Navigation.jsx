c7bf5dc9d61777cedd9d03b1a7fcd0fabf2b8f89
GiorgosTs <geontsou52@gmail.com>
2024-05-15 17:29:15 +0300

    Minor navigation button fixes

--- resources/js/Components/Navigation/Navigation.jsx
+++ resources/js/Components/Navigation/Navigation.jsx
@@ -148,10 +148,10 @@ export function Navigation() {
                                         </a>
                                     )}
                                 </div>
-                                    <Disclosure.Button key={'logout'} as="button" onClick={()=>console.log('Logout pressed')}
-                                                       className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">
-                                        Logout
-                                    </Disclosure.Button>
+                                {isLoggedIn && <Disclosure.Button key={'logout'} as="button" onClick={() => logout()}
+                                                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">
+                                    Logout
+                                </Disclosure.Button>}
                             </div>
                         </div>
                     </Disclosure.Panel>
