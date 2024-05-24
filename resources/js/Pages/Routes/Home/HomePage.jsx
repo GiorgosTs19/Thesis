077985996f7bc30d2d017b73269598ae4895a84e
GiorgosTs19 <geontsou52@gmail.com>
2024-05-24 05:09:13 +0300

    Added Logo

--- resources/js/Pages/Routes/Home/HomePage.jsx
+++ resources/js/Pages/Routes/Home/HomePage.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { array, arrayOf, number, shape, string } from 'prop-types';
 import Search from '@/Components/Search/Search.jsx';
 import HomeBanner from '@/Pages/Routes/Home/HomeBanner/HomeBanner.jsx';
+import logo from '@images/ThesisLogo.png';
 
 const HomePage = ({ mostCitationsUsers, mostWorksUsers, mostCitationsWorks }) => {
     return (
         <div className={styles.container}>
             <div className={styles.innerContainer}>
                 <div className={styles.wrapper}>
-                    <div className={styles.header}>MyPubsV2</div>
+                    <div className={styles.header}>
+                        <img src={logo} alt={'MyPubsV2'} className={'mx-auto my-5'} width={550} height={550} />
+                    </div>
                     <div className={'my-7 flex'}>
                         <Search isHomeScreen />
                     </div>
